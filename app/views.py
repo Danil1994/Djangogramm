f1cfcbbbe2ebfd27f0bcc6828f6dd94886b23ca6
Danil1994 <danilaleksandrovich36@gmail.com>
2023-09-11 13:00:08 +0300

    'login, auth'

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 from .models import User, Post, Photo, Like, Comment, Tag
 
 
@@ -8,10 +9,12 @@ def index(request):
     return render(request, 'index.html', context={'posts_list': posts_list})
 
 
+@login_required()
 def user_profile(request):
     return render(request, 'app/user_profile.html')
 
 
+@login_required()
 def post_create(request):
     return render(request, 'app/post_create.html')
 
--- djangogramm/settings.py
+++ djangogramm/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangogramm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+
+#while dev
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
